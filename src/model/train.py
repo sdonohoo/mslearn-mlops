35f4f59785505527077c9391b878c9ad7ff53ec0
Scott Donohoo <sdonohoo@microsoft.com>
2022-07-27 12:55:33 -0500

    lint fixes

--- src/model/train.py
+++ src/model/train.py
@@ -22,6 +22,7 @@ def main(args):
 
     # train model
     model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    return model
 
 
 def get_csvs_df(path):
@@ -41,9 +42,9 @@ def process_data(df):
 
     # train/test split
     X_train, X_test, y_train, y_test = train_test_split(X,
-        y,
-        test_size=0.30,
-        random_state=0)
+                                                        y,
+                                                        test_size=0.30,
+                                                        random_state=0)
 
     # return splits and encoder
     return X_train, X_test, y_train, y_test
